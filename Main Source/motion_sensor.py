04711c638881b82329301bcd3e2d7b1b21b68e64
Cesar Oros Olivas <156630201+CAOO16@users.noreply.github.com>
2025-05-07 18:54:53 -0600

    Update motion_sensor.py

--- Main Source/motion_sensor.py
+++ Main Source/motion_sensor.py
@@ -1,13 +1,13 @@
 import RPi.GPIO as GPIO       # Import Raspberry Pi GPIO library
 import time                   # Import time library for delays
 
-PIR_PIN = 17                  # BCM pin number where PIR output is connected
+PIR_PIN = 17                  
 
 def setup_pir():
-    GPIO.setup(PIR_PIN, GPIO.IN)      # Configure PIR_PIN as an input
-    print("Calibrating PIR sensor...")# Inform user calibration is starting
-    time.sleep(5)                      # Wait ~5 seconds for PIR warm-up
+    GPIO.setup(PIR_PIN, GPIO.IN)       # Configure PIR_PIN as an input
+    print("Calibrating PIR sensor...") # Inform user calibration is starting
+    time.sleep(5)                      # Wait 5 seconds for PIR warm-up
     print("PIR Ready")                 # Inform user PIR is ready
 
 def motion_detected():
-    return GPIO.input(PIR_PIN)         # Return True if PIR output is HIGH (motion), else False
+    return GPIO.input(PIR_PIN)         # Return True if PIR detects motion, False if not
