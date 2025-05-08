ed0ef7ce3af825a5a85740cde0ec035def57b46f
Cesar Oros Olivas <156630201+CAOO16@users.noreply.github.com>
2025-05-07 18:55:34 -0600

    Update texter.py

--- Main Source/texter.py
+++ Main Source/texter.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client     # Import Twilio REST client library
 
 account_sid = 'AC9807f1a9e1c43993ba857e0eca76a8b1'
-auth_token  = 'foo' #I cant have my token on a public website, the actaul token is filled out on the Pi's copy of this file
+auth_token  = 'foo' #I cant have my token on a public website, but the code on the Pi contains the actual token
 
 client = Client(account_sid, auth_token)
-# Instantiate the Twilio client with your Account SID and Auth Token
+# Instantiate the Twilio client with my Account SID and Auth Token
 
 FROM_NUMBER = '+16085645045'      # Twilio phone number (sender)
 TO_NUMBER   = '+17205619733'      # Destination phone number (recipient)
